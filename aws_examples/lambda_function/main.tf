39c3e6fbd0dec46f3caf0ec64125a360d6f975bc
romidm <de.meenta@gmail.com>
2021-04-27 21:45:28 -0400

    Create lambda function with python code

---
+++ aws_examples/lambda_function/main.tf
@@ -0,0 +1,54 @@
+/*
+  Lambda function with Python
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "test" {
+  function_name = "ServerlessTest"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "terraform-serverless"
+  s3_key    = "zip.zip" # https://github.com/romicofre/aws_lambda_py3_zip
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "index.lambda_handler"
+  runtime = "python3.8"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_test_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
